6e7ce233db35d7e9fb0645e54961e3f1cf5f4f29
Lilgibbsy <owen@gibbsbunch.com>
2022-12-09 14:46:39 -0700

    Start js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", animate);
+function animate() {
+  let playerMove = document.getElementById("playerMove");
+  let rndm = Math.round(Math.random() * 2);
+  if (rndm == 0) {
+  }
+}
